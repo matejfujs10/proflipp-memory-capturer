df055721bd843661aacf12f53e4d1c78d89e4ad5
matejfujs10 <sportnik.matejfujs@gmail.com>
2026-01-29 11:37:46 +0100

    Update main.tsx
    Add router and /combi route

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,14 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
+import Combi from "./pages/Combi";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/combi" element={<Combi />} />
+    </Routes>
+  </BrowserRouter>
+);
