2de321dc305f4eddcd6b96c9aeb4caf5a3e0cf8a
matejfujs10 <sportnik.matejfujs@gmail.com>
2026-01-29 13:02:13 +0100

    Update Combi.tsx
    update

--- src/pages/Combi.tsx
+++ src/pages/Combi.tsx
@@ -3,7 +3,33 @@ export default function Combi() {
     <main style={{ padding: 40 }}>
       <h1>Combi foto + video paket</h1>
       <p>
-        Profesionalna kombinacija fotografije in video vsebin za poroke in dogodke.
+        ✴️ TOP RENT – Tvoj športni kombi 5+1 za samo 50 €/dan! (po Sloveniji) 🛻💨
+
+✴️Najemi zdaj! info@proflipp.com ✴️
+
+✴️COMBI ali Camper -za dve osebi 🌄
+
+🔑 Simplicity is the key – preprosto, praktično, svobodno
+
+✴️Iščeš popoln pobeg iz vsakdanjika?
+✴️Ta kompaktni, športni kamper je ustvarjen za avanturiste, športnike in vse, ki iščejo svobodo na poti!
+
+✅ Parkiraš kjerkoli – brez kampov, brez komplikacij
+✅ umivalnik, hladilnik, miza, 12V električni priključki
+✅ Idealno za 2 osebi
+✅ Popoln za športnike – kolesarje, raziskuj in prenoči na prostem
+✅ Delaj od koderkoli – tvoja mobilna pisarna z razgledom
+✅ Vključeno brezplačno: žoga, badminton, loparji – športni rekviziti za zabavo
+✅ Majhna poraba goriva – potuj daleč, porabi malo
+✅ Možnost dostave na tvojo lokacijo (z doplačilom)
+
+🌍 Tvoj #vanlife se začne tukaj!
+Ne zamudi priložnosti – rezerviraj svoj termin še danes!
+
+📩 Piši zdaj info@proflipp.com in si zagotovi svoj športni kamper!
+
+#sportscamper #camperrental #outdooradventure #kombi #sport 
+#kamp #kamper
       </p>
       <p>
         Vsebina prihaja kmalu. Za povpraševanje me kontaktirajte.
