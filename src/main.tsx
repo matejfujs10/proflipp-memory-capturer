d96c427db047802972bdb84719ac2c5044d49b12
matejfujs10 <sportnik.matejfujs@gmail.com>
2026-01-29 11:53:28 +0100

    Update main.tsx
    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,5 @@
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
-import Combi from "./pages/Combi";
 import "./index.css";
 
-const rootElement = document.getElementById("root");
-
-if (!rootElement) {
-  throw new Error("Root element not found");
-}
-
-createRoot(rootElement).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<App />} />
-      <Route path="/combi" element={<Combi />} />
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById("root")!).render(<App />);
